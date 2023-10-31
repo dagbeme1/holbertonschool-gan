1dc80481a4dae901a2973cb69b481fcee73a8f05
Your Name <you@example.com>
2023-10-30 20:59:28 -0500

    mnist

---
+++ dcgan/data/tf_down.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+assert x_train.shape == (60000, 28, 28)
+assert x_test.shape == (10000, 28, 28)
+assert y_train.shape == (60000,)
+assert y_test.shape == (10000,)
